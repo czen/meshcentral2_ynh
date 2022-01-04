f908625b3e468c7786281e28e04d222c1e211c7c
Anton Bagliy <taccessviolation@gmail.com>
2022-01-04 04:10:44 +0300

    Initial commit with incomplete install script

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,13 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+# pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+
+# NodeJS version
+nodejs_version="14"
+
+# MeshCentral Version
+meshcentral_version="0.9.63"
 
 #=================================================
 # PERSONAL HELPERS
